d491bd55c29501031bb45a091b00c8e2b374e45e
Mirsaid Patarov <mirsaid@patarov.com>
2020-08-23 16:08:38 +0300

    Added Helper class

---
+++ app/Helpers/Helper.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Helpers;
+
+class Helper {
+  public static function slugify($text) {
+    $text = preg_replace('/&/', 'and', $text);
+    $text = preg_replace('/[^\pL\d]+/u', '-', $text);
+    $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+    $text = preg_replace('/[^-\w]+/', '', $text);
+    $text = trim($text, '-');
+    $text = preg_replace('/-+/', '-', $text);
+    $text = strtolower($text);
+
+    if (empty($text)) {
+      return 'n-a';
+    }
+
+    return $text;
+  }
+}
--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
+use App\Helpers\Helper;
 use App\Category;
 
 class CategorySeeder extends Seeder
@@ -24,27 +25,10 @@ class CategorySeeder extends Seeder
     foreach ($categories as $c) {
       $category = Category::create([
         'name' => $c,
-        'slug' => $this->slugify($c),
+        'slug' => Helper::slugify($c),
       ]);
 
       $category->save();
     }
-  }
-
-
-  private function slugify($text) {
-    $text = preg_replace('/&/', 'and', $text);
-    $text = preg_replace('/[^\pL\d]+/u', '-', $text);
-    $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
-    $text = preg_replace('/[^-\w]+/', '', $text);
-    $text = trim($text, '-');
-    $text = preg_replace('/-+/', '-', $text);
-    $text = strtolower($text);
-
-    if (empty($text)) {
-      return 'n-a';
-    }
-
-    return $text;
   }
 }
