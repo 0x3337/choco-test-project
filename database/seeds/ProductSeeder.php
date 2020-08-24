aa1f469e42f20683157926ebefbbc3e7f8858e88
Mirsaid Patarov <mirsaid@patarov.com>
2020-08-24 08:36:27 +0300

    Added products for Computers, TV and Headphones

--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -16,7 +16,26 @@ class ProductSeeder extends Seeder
   public function run()
   {
     $faker = Factory::create();
+
     $this->createIphones($faker);
+    $this->createMacs($faker);
+    $this->createTvs($faker);
+    $this->createHeadphones($faker);
+  }
+
+
+  private function createProduct($faker, $name, $categoryId, $minPrice, $maxPrice) {
+    $filename = Helper::slugify($name) . '.jpg';
+
+    $product = Product::create([
+      'name' => $name,
+      'price' => $faker->numberBetween($minPrice, $maxPrice),
+
+      'filename' => $filename,
+    ]);
+
+    $product->save();
+    $product->categories()->attach($categoryId);
   }
 
 
@@ -30,17 +49,40 @@ class ProductSeeder extends Seeder
     ];
 
     foreach ($iphones as $iphone) {
-      $filename = Helper::slugify($iphone) . '.jpg';
+      $this->createProduct($faker, $iphone, 1, 399, 999);
+    }
+  }
 
-      $product = Product::create([
-        'name' => $iphone,
-        'price' => $faker->numberBetween(399, 999),
+  private function createMacs($faker) {
+    $macs = [
+      'MacBook Pro 2019',
+      'MacBook Air 2020',
+    ];
 
-        'filename' => $filename,
-      ]);
+    foreach ($macs as $mac) {
+      $this->createProduct($faker, $mac, 2, 999, 1999);
+    }
+  }
 
-      $product->save();
-      $product->categories()->attach(1);
+  private function createTvs($faker) {
+    $tvs = [
+      'Samsung QE65Q800TAUXCE QLED 8K',
+    ];
+
+    foreach ($tvs as $tv) {
+      $this->createProduct($faker, $tv, 3, 1999, 3999);
+    }
+  }
+
+  private function createHeadphones($faker) {
+    $headphones = [
+      'AirPods Pro',
+      'Powerbeats Pro',
+      'Monster Beats',
+    ];
+
+    foreach ($headphones as $headphone) {
+      $this->createProduct($faker, $headphone, 4, 50, 300);
     }
   }
 }
